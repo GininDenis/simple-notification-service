fa844c91c5e618c38bd61564cafe4a06147961fe
Denis Ginin <ginin.denis@nicecode.biz>
2019-01-09 14:40:37 +0300

    #4 Refactoring
    * added form labels with translation
    * email validation change

--- src/apps/auth/forms.py
+++ src/apps/auth/forms.py
@@ -1,21 +1,24 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    first_name = forms.CharField(max_length=30, required=False,
+                                 help_text=_('Optional.'), label=_('First name'))
+    last_name = forms.CharField(max_length=30, required=False,
+                                help_text=_('Optional.'), label=_('Last name'))
+    email = forms.EmailField(max_length=254, help_text=_(
+        'Required. Inform a valid email address.'), label=_('Email address'))
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1',
+                  'password2',)
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
-        username = self.cleaned_data.get('username')
-        if email and User.objects.filter(email=email).exclude(
-                username=username).exists():
-            raise forms.ValidationError(u'Email addresses must be unique.')
+        if email and User.objects.filter(email=email).exists():
+            raise forms.ValidationError(_(u'Email addresses must be unique.'))
         return email
