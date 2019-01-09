711a83f44be1fc14b0dc61b9c88e9bbc8c6b44dc
Denis Ginin <ginin.denis@nicecode.biz>
2019-01-09 15:29:46 +0300

    #4 Activation url update

--- src/apps/auth/urls.py
+++ src/apps/auth/urls.py
@@ -4,6 +4,6 @@ from apps.auth.views import SignUpView, ActivateAccountView
 
 urlpatterns = [
     path('register/', SignUpView.as_view(), name='signup'),
-    path('activate/(<str:uidb64>)/(<str:token>)/',
+    path('activate/<str:uidb64>/<str:token>/',
         ActivateAccountView.as_view(), name='activate'),
 ]
