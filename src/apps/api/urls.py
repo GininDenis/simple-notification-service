e9a4fec3cf71ac7733f94b049e5ec33c435498fb
Denis Ginin <ginin.denis@nicecode.biz>
2019-01-21 12:47:34 +0300

    #14 Refactoring

--- src/apps/api/urls.py
+++ src/apps/api/urls.py
@@ -8,14 +8,12 @@ from apps.api.views import (
 router = routers.DefaultRouter()
 router.register('subscription', SubscriptionViewSet, base_name='subscription')
 
-
 app_name = 'api'
 
 urlpatterns = [
 
     path('', include(router.urls)),
-    path('test/endpoint/',
-         TestEndpointView.as_view(), name='test-endpoint'),
+    path('test/endpoint/', TestEndpointView.as_view(), name='test-endpoint'),
     path('confirm/', ConfirmApiView.as_view(), name='confirm'),
 
 ]
