bc06c0e6d2c7bb4bf0bc92bdaef1a9dc6a33f8be
Denis Ginin <ginin.denis@nicecode.biz>
2019-01-28 13:00:18 +0300

    #15 Tasks tests complete
    * REquirements update

--- src/apps/notifications/tests/test_tasks.py
+++ src/apps/notifications/tests/test_tasks.py
@@ -1,3 +1,5 @@
+import json
+
 from collections import namedtuple
 
 from django.test import TestCase, override_settings
@@ -5,7 +7,8 @@ from django.contrib.sites.models import Site
 from model_mommy import mommy
 from mock import patch
 
-from apps.notifications.tasks import get_endpoint, create_message
+from apps.notifications.tasks import get_endpoint, create_message, \
+    send_subscription_confirmation
 from apps.notifications.models import Subscription
 
 ENDPOINT_URL = 'http://test.test/test/'
@@ -35,3 +38,15 @@ class SubscriptionConfirmationTest(TestCase):
         self.assertEqual(instance, self.subscription)
         self.assertEqual(message['token'], self.subscription.token)
         self.assertEqual(message['subscription_id'], self.subscription.pk)
+
+    @patch('apps.notifications.tasks.requests.post')
+    def test_send_subscription_confirmation(self, mock_post):
+        error_msg = lambda : {'Status': 'Error'}
+        response = namedtuple('response', 'status_code, json')
+        response.status_code = 400
+        response.json = error_msg
+        mock_post.return_value = response
+        send_subscription_confirmation(self.subscription.id)
+        self.subscription.refresh_from_db()
+        self.assertEqual(self.subscription.error_msg, json.dumps(error_msg()))
+        
