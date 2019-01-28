9aa1458c9d75a8afd3738685ae5371c084279d5c
Denis Ginin <ginin.denis@nicecode.biz>
2019-01-28 15:22:29 +0300

    #15 Added views tests

--- src/apps/notifications/tests/test_forms.py
+++ src/apps/notifications/tests/test_forms.py
@@ -1,22 +1,22 @@
 from django.test import TestCase
-from django.db.models.signals import post_save
+from model_mommy import mommy
 
 from apps.notifications.forms import SubscriptionUpdateForm
+from apps.notifications.models import Topic
+from apps.users.models import User
 
 
 class SubscriptionUpdateFormTest(TestCase):
 
-    TEST_DATA = {
-        'email': 'test@test.test',
-        'password': 'testpassword',
-        'first_name': 'John',
-        'last_name': 'Smith'
-    }
 
     def setUp(self):
-        # TODO: Create some amount of topics and users
-        pass
+        for user in mommy.make(User, _quantity=3):
+            mommy.make(Topic,owner=user, _quantity=3)
+
 
     def test_init(self):
-        # form = SubscriptionUpdateForm()
-        pass
+        user = User.objects.first()
+        form = SubscriptionUpdateForm(user=user)
+
+        self.assertEqual(list(form.fields['topic'].queryset),
+                         list(Topic.objects.filter(owner__id=user.pk)))
