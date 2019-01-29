8864ee06459404cd90158f2cc3cbd1fcce714f62
Denis Ginin <ginin.denis@nicecode.biz>
2019-01-29 16:43:44 +0300

    #15 Added api test

--- src/apps/api/tests/test_views.py
+++ src/apps/api/tests/test_views.py
@@ -1,15 +1,23 @@
+from collections import namedtuple
+
 from django.test import TestCase
 from model_mommy import mommy
 
-from apps.notifications.forms import SubscriptionUpdateForm
-from apps.notifications.models import Topic
-from apps.users.models import User
+from apps.notifications.models import Subscription
+from apps.api.views import ConfirmSubscriptionApiView
 
 
 class ConfirmSubscriptionApiTestCase(TestCase):
 
-    def test_view(self):
-
-
-
-        pass
+    def test_post(self):
+        subscription = mommy.make(Subscription)
+        view = ConfirmSubscriptionApiView()
+        request = namedtuple('request', 'data')
+        request.data = {
+            'subscription': subscription.pk,
+            'token': subscription.token
+        }
+        view.post(request)
+        subscription.refresh_from_db()
+        self.assertEqual(subscription.status,
+                         Subscription.STATUS_CHOICES.confirmed)
