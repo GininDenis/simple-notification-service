fb03ff788cd6ae7292f65040338f01f0b983bd8e
Denis <ginindenis@gmail.com>
2019-02-27 05:23:49 +0000

    #17 Added viewsets * Added tests for subscription viewsset

--- src/apps/api/serializers.py
+++ src/apps/api/serializers.py
@@ -1,29 +1,25 @@
+from django.contrib.auth import authenticate
+from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
-from django.utils.translation import ugettext_lazy as _
 
-from apps.notifications.models import Subscription
+from apps.notifications.models import Subscription, Topic
+from apps.users.models import User
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subscription
         fields = ['protocol', 'endpoint', 'topic', 'id']
-        extra_kwargs = {
-            'protocol': {
-                'write_only': True
-            },
-            'endpoint': {
-                'write_only': True
-            },
-            'topic': {
-                'write_only': True
-            }
-        }
+
+
+class TopicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Topic
+        fields = ['id', 'title', 'owner']
 
 
 class ConfirmationSerializer(serializers.Serializer):
-
     subscription = serializers.PrimaryKeyRelatedField(
         queryset=Subscription.objects.all()
     )
@@ -36,3 +32,23 @@ class ConfirmationSerializer(serializers.Serializer):
         if subscription.token != token:
             raise ValidationError({'token': _('Invalid token')})
         return attrs
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(max_length=128)
+
+    def validate(self, attrs):
+
+        user = authenticate(**attrs)
+        if user:
+            attrs.update({'user': user})
+            return attrs
+        else:
+            raise ValidationError('Invalid user')
+
+
+class RestoreSessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'email']
